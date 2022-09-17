c47f646ade45e8929d41d7e25227c05c651a7320
marcosom8 <111856760+marcosom8@users.noreply.github.com>
2022-09-16 17:17:27 -0700

    subiendo

---
+++ Userscontacts/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+
+
+const routes: Routes = [
+  { path: '', component: ListUsersComponent },
+  { path: 'list-users', component: ListUsersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
